29479e628967191882fb59a01a9876fc1bc61513
Mohamed el haia <elhaia.mohamed555@gmail.com>
2025-03-11 16:33:04 +0000

    needs some tiny edits

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: moel-hai <moel-hai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 21:23:55 by moel-hai          #+#    #+#             */
-/*   Updated: 2025/02/08 12:58:44 by moel-hai         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:32:46 by moel-hai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,15 @@ void	send_signal(int pid, char c)
 	int	i;
 
 	i = 0;
-	while (i++ < 8)
+	while (i <= 7)
 	{
-		if (((c >> (8 - i)) & 1) == 1)
+		if (((c >> (7 - i)) & 1) == 1)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
 		usleep(200);
 		usleep(200);
+		i++;
 	}
 }
 
