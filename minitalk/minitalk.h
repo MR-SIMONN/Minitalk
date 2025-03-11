41a327c17290aed1a979bcc9a59bfc2dde71cea0
Mohamed el haia <elhaia.mohamed555@gmail.com>
2025-03-11 16:37:51 +0000

    needs some tiny edits

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: moel-hai <moel-hai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 21:32:01 by moel-hai          #+#    #+#             */
-/*   Updated: 2025/02/05 21:32:27 by moel-hai         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:35:35 by moel-hai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_atoi(const char *str);
-int	ft_printf(const char *format, ...);
-int	ft_putchar(char c, size_t *counter);
-int	ft_putnbr(int num, size_t *counter);
-int	ft_puthex(unsigned long num, char format, size_t *counter);
-int	ft_putaddr(unsigned long n, size_t *counter);
-int	ft_putstr(char *str, size_t *counter);
-int	ft_putunsigned(unsigned int n, size_t *counter);
+int     ft_printf(const char *str, ...);
+int	    ft_putchar(char c);
+void	ft_putnbr(int n, int *i);
+int	    ft_putstr(char *s);
+long    ft_atoi(const char	*str);
+
 #endif
